5ed5d23b08173b33e534602a6206494de95a2868
Diegoxlus <lusky1996@gmail.com>
2020-09-30 21:19:13 +0200

    Finish first search #12

--- src/test/java/code/SearchesTest.java
+++ src/test/java/code/SearchesTest.java
@@ -5,12 +5,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SearchesTest {
+ class SearchesTest {
     @Test
     void findFirstDecimalFractionByUserName(){
         assertEquals(2.0/1, new Searches().findFirstDecimalFractionByUserName("Ana"));
         assertEquals(0.0/1, new Searches().findFirstDecimalFractionByUserName("Oscar"));
-
     }
 
 }
